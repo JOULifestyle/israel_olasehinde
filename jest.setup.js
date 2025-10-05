b0208a8b392637a3c4f00deabb3a8c44a1058fc9
Israel Olasehinde <olasehindeisrael@gmail.com>
2025-10-06 00:00:38 +0100

    test asynchronous fixed

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,6 @@ process.env.NODE_ENV = "test";
 
 // Silence dotenv logs
 process.env.DOTENV_LOG_LEVEL = "silent";
+
+// Mock ioredis globally to prevent real Redis connections in tests
+jest.mock("ioredis");
